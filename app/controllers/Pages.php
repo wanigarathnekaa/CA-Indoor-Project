a4f0ceec790c1c5333d11ce4164619208b8aa25e
unknown <2021cs207@stu.ucsc.cmb.ac.lk>
2023-10-30 01:18:32 +0530

    Adding Advertisements

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -58,6 +58,20 @@ class Pages extends Controller{
 
         $this->view('Pages/UserProfiles/editProfile');
     }
+
+    public function Advertisements($name){
+        // $user = $this->pagesModel->findUser($_SESSION['user_email']);
+        // print_r($user);
+
+        $this->view('Pages/Advertisement/advertisement');
+    }
+
+    public function AdvertisementDetails($name){
+        // $user = $this->pagesModel->findUser($_SESSION['user_email']);
+        // print_r($user);
+
+        $this->view('Pages/Advertisement/advertisementDetails');
+    }
 }
 
 ?>
