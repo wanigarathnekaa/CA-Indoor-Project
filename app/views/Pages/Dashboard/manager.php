eb5137bc085d1e5f1d6ee7b6228ee3be9246ac13
MilanBhanuka <vgmbhanuka@gmail.com>
2023-12-14 04:06:49 +0530

    changed dashboard add reservation table for thye dashboard

--- app/views/Pages/Dashboard/manager.php
+++ app/views/Pages/Dashboard/manager.php
@@ -5,16 +5,14 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Manager Dashboard</title>
+
+      <!-- Stylesheets -->
       <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/managerDashboard.css">
-      <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/sideBar.css">
       
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
-
-
 </head>
 
 <body>
-
       <!-- Sidebar -->
       <?php
       $role = "Manager";
@@ -33,7 +31,7 @@
                               <div class="cardName">Coaches</div>
                         </div>
                         <div class="iconBx">
-                              <i class="fa-solid fa-person"></i>
+                              <i class="fa-solid fa-user-group"></i>
                         </div>
                   </a>
 
@@ -43,7 +41,7 @@
                               <div class="cardName">Players</div>
                         </div>
                         <div class="iconBx">
-                              <i class="fa-solid fa-person"></i>
+                              <i class="fa-solid fa-users"></i>
                         </div>
                   </a>
 
@@ -89,11 +87,30 @@
                   </a>
             </div>
 
-            <!-- Calander -->
-            <?php
-            require APPROOT . '/views/Pages/Calendar/calender.php';
-            ?>
 
+            <div class="details">
+
+                  <!-- Recent Reservations -->
+                  <div class="tablediv">
+                        <?php
+                              require APPROOT . '/views/Pages/Tables/dailyReservation.php';
+                        ?>
+                  </div>
+                  
+                  
+                  <!-- Calander -->
+                  <div class="calanderdiv">
+                        <!-- <?php
+                        require APPROOT . '/views/Pages/Calendar/calender.php';
+                        ?> -->
+                  </div>
+                  
+
+            </div> 
+
+            
+
+            <!-- chart -->
             <div style="text-align: center; margin-top: 40px;">
                   <div id="chartContainer" style="height: 370px; width: 90%; margin: 0 auto;"></div>
             </div>
@@ -101,6 +118,7 @@
       </section>
 
 
+      <!-- javascripts -->
       <script src="<?php echo URLROOT; ?>/js/sideBar.js"></script>
       <script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
       <script src="<?php echo URLROOT; ?>/js/chart.js"></script>
---
+++ app/views/Pages/Tables/dailyReservation.php
@@ -0,0 +1,116 @@
+<html>
+
+<head>
+    <meta name="viewport" content="width = device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo URLROOT;?>/css/dailyReservation.css">
+</head>
+
+<body>
+<div class="recentOrders">
+      <div class="cardHeader">
+            <!-- date -->
+            <h2>2023-12-14</h2>
+            <!-- veiw all button -->
+            <a href="#" class="btn">View All</a>
+      </div>
+
+      <table>
+            <!-- table header -->
+            <thead>
+            <tr>
+                  <td>Customer</td>
+                  <td>Time Slot</td>
+                  <td>Net</td>
+                  <td>Status</td>
+            </tr>
+            </thead>
+
+            <!-- table body -->
+            <tbody>
+            <tr>
+                  <td>Milan Bhanuka</td>
+                  <td>07AM-09AM</td>
+                  <td>A</td>
+                  <td><span class="status paid">Paid</span></td>
+            </tr>
+
+            <tr>
+                  <td>Kaveesha Wanigarathne</td>
+                  <td>10AM-12PM</td>
+                  <td>Machine</td>
+                  <td><span class="status confirmation">confirmation fee</span></td>
+            </tr>
+
+            <tr>
+                  <td>Hasini Hewa</td>
+                  <td>02PM-04PM</td>
+                  <td>Paid</td>
+                  <td><span class="status canceled">Canceled</span></td>
+            </tr>
+
+            <tr>
+                  <td>Milan Bhanuka</td>
+                  <td>07AM-09AM</td>
+                  <td>A</td>
+                  <td><span class="status paid">Paid</span></td>
+            </tr>
+
+            <tr>
+                  <td>Kaveesha Wanigarathne</td>
+                  <td>10AM-12PM</td>
+                  <td>Machine</td>
+                  <td><span class="status confirmation">confirmation fee</span></td>
+            </tr>
+
+            <tr>
+                  <td>Hasini Hewa</td>
+                  <td>02PM-04PM</td>
+                  <td>Paid</td>
+                  <td><span class="status canceled">Canceled</span></td>
+            </tr>
+
+            <tr>
+                  <td>Milan Bhanuka</td>
+                  <td>07AM-09AM</td>
+                  <td>A</td>
+                  <td><span class="status paid">Paid</span></td>
+            </tr>
+
+            <tr>
+                  <td>Kaveesha Wanigarathne</td>
+                  <td>10AM-12PM</td>
+                  <td>Machine</td>
+                  <td><span class="status confirmation">confirmation fee</span></td>
+            </tr>
+
+            <tr>
+                  <td>Hasini Hewa</td>
+                  <td>02PM-04PM</td>
+                  <td>Paid</td>
+                  <td><span class="status canceled">Canceled</span></td>
+            </tr>
+            <tr>
+                  <td>Milan Bhanuka</td>
+                  <td>07AM-09AM</td>
+                  <td>A</td>
+                  <td><span class="status paid">Paid</span></td>
+            </tr>
+
+            <tr>
+                  <td>Kaveesha Wanigarathne</td>
+                  <td>10AM-12PM</td>
+                  <td>Machine</td>
+                  <td><span class="status confirmation">confirmation fee</span></td>
+            </tr>
+
+            <tr>
+                  <td>Hasini Hewa</td>
+                  <td>02PM-04PM</td>
+                  <td>Paid</td>
+                  <td><span class="status canceled">Canceled</span></td>
+            </tr>
+
+            </tbody>
+      </table>
+</div>
+</body>
