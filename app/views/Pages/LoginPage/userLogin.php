82a7e9f674db0b9debc12d3847c7b96cadd9ac21
MilanBhanuka <vgmbhanuka@gmail.com>
2023-12-26 17:18:34 +0530

    change login page and register page and romove header and footer of register page

--- app/views/Pages/LoginPage/userLogin.php
+++ app/views/Pages/LoginPage/userLogin.php
@@ -1,31 +1,47 @@
-            <div class="container">
-                <h1>Login</h1>
-                
-                <div class="input-box">
-                    <!-- <label for="uname"><b>Username</b></label> -->
-                    <input type="text" name="email" id="email" placeholder="Email" value="<?php echo $data['email']; ?>">
-                    <i class='bx bxs-user'></i>
-                    <span class="form-invalid"><?php echo $data['email_err']; ?></span>
-                </div>
-                
-                <div class="input-box">
-                    <!-- <label for="psw"><b>Password</b></label> -->
-                    <input type="password" name="pwd" id="pwd" placeholder="Password" value="<?php echo $data['pwd'];?>">
-                    <i class='bx bxs-low-vision'></i>
-                    <span class="form-invalid"><?php echo $data['pwd_err']; ?></span>
-                </div>
-                <div class="remember-forget">
-                    <label><input type="checkbox" checked="checked" name="remember"> Remember me</label>
-                    <a href="#">Forgot password?</a><br>
-                </div>
-            
-                <button class="btn" type="submit">Login</button>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo URLROOT;?>/css/loginStyle.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Login Page</title>
+</head>
+<body>          
+      <div class="container" id="container">
+            <div class="sign-in-container">
+                  <form  action="<?php echo URLROOT;?>/Users/login" method="POST">
+                        <h2>Login</h2>
+                        <div class="input-box">
+                              <input type="email"  name="email" id="email" value="<?php echo $data['email']; ?>">
+                              <label>Email</label>
+                              <span class="form-invalid"><?php echo $data['email_err']; ?></span>
+                        </div>
+                        <div class="input-box">
+                              <input type="password" name="pwd" id="pwd" value="<?php echo $data['pwd'];?>">
+                              <label>Password</label>
+                              <span class="form-invalid"><?php echo $data['pwd_err']; ?></span>
+                        </div>
+                        <div class="forgot-password">
+                              <label><input type="checkbox" checked="checked" name="remember"> Remember me</label>
+                              <a href="#">Forgot Password?</a>
+                        </div>
+                        <button class="btn" type="submit">Login</button>
 
-                <?php 
-                    if($role == "User"){
-                        echo '<div class="registerLink">
-                                <p>Don\'t have an account?</p>
-                                <a href="register">Register</a>
-                            </div>';
-                    }
-                ?>
+                        <?php 
+                              if($role == "User"){
+                                    echo '<div class="signup-link">
+                                           <p>Don\'t have an account?</p>
+                                           <a href="register">Register</a>
+                                          </div>';
+                               }
+                         ?>
+                  </form>
+            </div>
+            <div class="imgcontainer">
+                  <img src="<?php echo URLROOT;?>/images/Logo3.png" alt="logo" class="logo">
+                  
+            </div>
+      </div>
+</body>
+</html>
