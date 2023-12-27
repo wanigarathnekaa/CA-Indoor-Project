d85e53e018ac56e445876b1f0d26cbb0fcbe0b0c
MilanBhanuka <vgmbhanuka@gmail.com>
2023-12-28 02:13:26 +0530

    coach table changed

--- app/views/Pages/Tables/coaches_Table.php
+++ app/views/Pages/Tables/coaches_Table.php
@@ -24,7 +24,23 @@
       <!-- Content -->
       <section class="home">
 
-            <!-- Coaches Table -->
+            <!-- Table Topic -->
+            <div class="table-topic">
+                  <div class="topic-name">
+                        <h1>Coachers : 25 </h1>
+                  </div>
+                  <div class="search">
+                        <label>
+                              <input type="text" placeholder="Search here">
+                              <i class="fa-solid fa-magnifying-glass icon"></i>
+                        </label>
+                  </div>
+                  <div class="add-btn">
+                        <a href="<?php echo URLROOT;?>/Coach/register"><i class="fa-solid fa-user-plus  icon"></i></a>
+                  </div>
+            </div>
+
+            <!-- Table -->
             <div class="table-container">
                   <table >
                         <!-- table header -->
@@ -43,6 +59,17 @@
 
                         <!-- table body -->
                         <tbody>
+                              <tr onclick="window.location.href='#';">
+                                    <td>Milan Bhanuka</td>
+                                    <td>vgmbhanuka@gmail.com</td>
+                                    <td>0716720864</td>
+                                    <td>1 year year experience in coaching</td>
+                                    <td>level1, sara player, devision3 player</td>
+                                    <td>lavel4 cricket coach</td>
+                                    <td><a href="#"><i class="fa-solid fa-user-pen edit icon"></i></a></td>
+                                    <td><a href="#"><i class="fa-solid fa-user-slash delete icon"></i><a></td>
+                                    
+                              </tr>
                               <tr onclick="window.location.href='#';">
                                     <td>Milan Bhanuka</td>
                                     <td>vgmbhanuka@gmail.com</td>
