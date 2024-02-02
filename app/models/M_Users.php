46f65b162bb51b09f2111906a498e0c69e7b831c
Hasini Navodya <nivodya2001@gmail.lk>
2024-02-03 00:57:51 +0530

    "Coach password send via Email"

--- app/models/M_Users.php
+++ app/models/M_Users.php
@@ -68,7 +68,21 @@ class M_Users
         }
 
     }
+    public function loginCoach($email, $password)
+    {
+        $this->db->query('SELECT * FROM coaches WHERE email = :email');
+        $this->db->bind(':email', $email);
 
+        $row = $this->db->single();
+
+        $hashed_password = $row->password;
+        if (password_verify($password, $hashed_password)) {
+            return $row;
+        } else {
+            return false;
+        }
+
+    }
     public function loginManager($email, $password)
     {
         $this->db->query('SELECT * FROM managers WHERE email = :email');
