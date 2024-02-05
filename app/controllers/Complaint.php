375a309cfea7145d6ba397dd8cf576f9c610ee71
wanigarathnekaa <kavisha.abhishake@gmail.com>
2024-02-05 22:05:53 +0530

    brand category name adjustment

--- app/controllers/Complaint.php
+++ app/controllers/Complaint.php
@@ -1,98 +1,99 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
- 
-class Complaint extends Controller{
+
+class Complaint extends Controller
+{
     private $complaintModel;
-    public function __construct(){
-        $this->complaintModel=$this->model('M_Complaint');
+    public function __construct()
+    {
+        $this->complaintModel = $this->model('M_Complaint');
 
     }
-   
-    public function create(){
-         if($_SERVER['REQUEST_METHOD']=='POST'){
+
+    public function create()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-           
+
             //input data
-            $data=[
-                'name'=>trim($_POST['name']),
-                'email'=>trim($_POST['email']),
-                'message'=>trim($_POST['message']),
+            $data = [
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'message' => trim($_POST['message']),
 
-                
 
-             
 
-                'name_err'=>'',
-                'email_err'=>'',
-                'message_err'=>''
+
+
+                'name_err' => '',
+                'email_err' => '',
+                'message_err' => ''
 
             ];
-               
-            if(empty($data['name'])) {
-                $data['name_err']='Please enter the name';
+
+            if (empty($data['name'])) {
+                $data['name_err'] = 'Please enter the name';
             }
-            
-            if(empty($data['email'])) {
-                $data['email_err']='Please enter the email';
+
+            if (empty($data['email'])) {
+                $data['email_err'] = 'Please enter the email';
             }
-            if(empty($data['message'])) {
-                $data['message_err']='Please enter the message';
+            if (empty($data['message'])) {
+                $data['message_err'] = 'Please enter the message';
             }
-            if(empty($data['name_err']) && empty($data['email_err'])&&empty($data['message_err'])){
-                if($this->complaintModel->create($data)){
+            if (empty($data['name_err']) && empty($data['email_err']) && empty($data['message_err'])) {
+                if ($this->complaintModel->create($data)) {
                     // flash('complaint_created');
                     redirect('Pages/Dashboard/user');
-                }
-                else{
+                } else {
                     die('Somthing went wrong');
                 }
-            }
-            else{
+            } else {
                 //load view
-                $this->view('Components/contactUs',$data);
-                       
+                $this->view('Components/contactUs', $data);
+
             }
-          }
-          else{
-                      $data=[
-                          'name'=>'',
-                          'email'=>'',
-                          'message'=>'',
-                            
-            
-                             'name_err'=>'',
-                             'email_err'=>'',
-                             'message_err'=>''
-                            
-            
-                         ];
-                         //load view
-                         $this->view('Components/contactUs',$data);
-                       
-                     }}
+        } else {
+            $data = [
+                'name' => '',
+                'email' => '',
+                'message' => '',
 
 
-                    //  view complaints
-                     public function viewComplaints()
-                     {
-                         $complaints = $this->complaintModel->getComplaints();
-                         
-                         $data = [
-                             'complaints' => $complaints
-                         ];
-                         $this->view('Pages/Complaint/view_complaint', $data);
-                     }
-                     public function ComplaintDetails($complaintId)
-                     {
-                         $complaint = $this->complaintModel->getComplaintById($complaintId);
-                         
-                         $data = [
-                            'complaint' => $complaint,
-                         ];
-                         $this->view('Pages/Complaint/complaintDetails', $data);
-                     }
-// Function to send email with password
+                'name_err' => '',
+                'email_err' => '',
+                'message_err' => ''
+
+
+            ];
+            //load view
+            $this->view('Components/contactUs', $data);
+
+        }
+    }
+
+
+    //  view complaints
+    public function viewComplaints()
+    {
+        $complaints = $this->complaintModel->getComplaints();
+
+        $data = [
+            'complaints' => $complaints
+        ];
+        $this->view('Pages/Complaint/view_complaint', $data);
+    }
+    public function ComplaintDetails($complaintId)
+    {
+        $complaint = $this->complaintModel->getComplaintById($complaintId);
+
+        $data = [
+            'complaint' => $complaint,
+        ];
+        $this->view('Pages/Complaint/complaintDetails', $data);
+    }
+    // Function to send email with password
     public function sendEmail($complaintId)
     {
         require_once APPROOT . '/libraries/phpmailer/src/PHPMailer.php';
@@ -100,11 +101,11 @@ class Complaint extends Controller{
         require_once APPROOT . '/libraries/phpmailer/src/Exception.php';
         if (isset($_POST["send"])) {
 
-        $mail = new PHPMailer(true);
+            $mail = new PHPMailer(true);
 
-        $email=$this->complaintModel->getComplaintEmailById($complaintId);
+            $email = $this->complaintModel->getComplaintEmailById($complaintId);
 
- 
+
             //Server settings
             $mail->isSMTP();
             $mail->Host = 'smtp.gmail.com';
@@ -113,7 +114,7 @@ class Complaint extends Controller{
             $mail->Password = 'ndvpqhmangzegxhn';
             $mail->SMTPSecure = 'ssl';
             $mail->Port = 465;
-            
+
             //Recipients
             $mail->setFrom('nivodya2001@gmail.com', 'Hasini Hewa');
             $mail->addAddress($email);
@@ -121,31 +122,31 @@ class Complaint extends Controller{
             //Content
             $mail->isHTML(true);
             $mail->Subject = 'About your complaint';
-            $mail->Body    = $_POST["message"];
+            $mail->Body = $_POST["message"];
             $mail->send();
 
             redirect('Pages/Dashboard/admin');
 
 
             if ($mail->send()) {
-                if($this->complaintModel->delete($complaintId)){
+                if ($this->complaintModel->delete($complaintId)) {
                     redirect('Pages/Dashboard/admin');
 
-                }
-                else{
+                } else {
                     die('somthing wrong');
-                }                
+                }
 
 
 
-                
+
             } else {
                 // Handle email sending failure
                 echo 'Email could not be sent.';
             }
-        } 
+        }
 
-    }}
-                         
+    }
+}
+
 
 ?>
