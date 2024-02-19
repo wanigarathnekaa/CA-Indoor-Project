8bc5ab9f2b696be20cbc00895c2792f7e1b58a0e
MilanBhanuka <vgmbhanuka@gmail.com>
2024-02-19 23:07:46 +0530

    Manage profile,edit manage profile

--- app/views/Pages/Manager/managerEditProfile.php
+++ app/views/Pages/Manager/managerEditProfile.php
@@ -6,64 +6,96 @@
     <title>Edit Profile</title>
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/editProfileStyle.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <body>
-    <div class="container">
-        <h1 class="form-title">Edit profile</h1>
-        <form action="<?php echo URLROOT; ?>/Manager/edit" method="POST">
-            <img src="<?php echo URLROOT; ?>/images/user.png" class="picture"><button class="btn1"><u>Change/Delete
-                    Profile Picture</u></button>
-                <div class="main-user-info">
-                    <div class="user-input-box">
-                        <label for="fullName">Name:</label>
-                        <input type="text" id="name" name="name" placeholder="Enter Name" value="<?=$data["name"]?>"/>
-                    </div>
-                    <div class="user-input-box">
-                        <label for="email">Email:</label>
-                        <input type="email" id="email" name="email" placeholder="Enter Email" value="<?=$data["email"]?>"/>
-                    </div>
-                    <div class="user-input-box">
-                        <label for="phoneNumber">Phone Number:</label>
-                        <input type="text" id="phoneNumber" name="phoneNumber" placeholder="Enter Phone Number" value="<?=$data["phoneNumber"]?>"/>
-                    </div>
-                    <div class="user-input-box">
-                        <label for="NIC">NIC:</label>
-                        <input type="NIC" id="nic" name="nic" placeholder="Enter NIC" value="<?=$data["nic"]?>"/>
-                    </div>
-                    <div class="user-input-box">
-                        <label for="Address">Address:</label>
-                        <input type="Address" id="address" name="address" placeholder="Enter Address" value="<?=$data["address"]?>"/>
+    <?php
+        $role = $_SESSION['user_role'];
+        require APPROOT . '/views/Pages/Dashboard/header.php';
+        require APPROOT . '/views/Components/Side Bars/sideBar.php';
+        
+    ?>
+
+
+      <!-- content -->
+    <section class="home">
+        <div class="container">
+            <div class="head">
+                <h1 class="form-title">Edit profile</h1> 
+            </div>
+
+            <!-- form details -->
+            <div class="formbox">
+                <form action="<?php echo URLROOT; ?>/Manager/edit" method="POST" enctype="multipart/form-data">
+                    <!-- profile picture -->
+                    <div class="profilepic">
+                        <label for="file" class="propiclabel">
+                            <div class="profile-pic">
+                                <!-- <span class="camicon"><i class="fa-solid fa-camera"></i></span> -->
+                                <span class="camicon"><i class='bx bxs-camera-plus'></i></span>
+                                <?php
+                                    if($data["img"] == null){
+                                        echo '<img class="profile-image" id="output" src="'.URLROOT.'/public/profilepic/avatar.jpg">' ;
+                                    }
+                                    else{
+                                        echo '<img class="profile-image" id="output" src="'.URLROOT.'/public/profilepic/'.$data["img"].'">'; 
+                                    }
+                                ?>
+                            </div>
+                        </label>
+                        <input type="file" hidden name="file" id="file" onchange="loadFile(event)" value="<?= URLROOT ?>/public/profilepic/<?= $data["img"] ?>" />     
                     </div>
 
-                <div class="form-submit-btn">
-                    <input type="submit" name="submit" class="btn2" value="Save">
-                    <a href="<?php echo URLROOT; ?>/Pages/Manager_Delete_Profile/manager"><input name="submit" class="btn3" value="Delete profile"></a>
+                    <!-- profile details -->
+                    <div class="profileDetails">
+                        <div class="box">
+                            <label for="name">Name</label>
+                            <input type="text" id="name" name="name"  value="<?=$data["name"]?>" required/>
+                        </div>
+                        <div class="box">
+                            <label for="email">Email</label>
+                            <input type="email" id="email" name="email" value="<?=$data["email"]?>" />
+                        </div>
+                        <div class="box">
+                            <label for="phoneNumber">Phone Number</label>
+                            <input type="text" id="phoneNumber" name="phoneNumber"  value="<?=$data["phoneNumber"]?>"/>
+                        </div>
+                        <div class="box">
+                            <label for="nic">NIC</label>
+                            <input type="text" id="nic" name="nic" placeholder="Enter NIC" value="<?=$data["nic"]?>"/>
+                        </div>
+                        
+                        <!-- <div class="box">
+                            <label for="srtAddress">Street Address</label>
+                            <input type="text" id="srtAddress" name="srtAddress" value="<?=$data["srtAddress"]?>"/>
+                        </div>
+                        <div class="box">
+                            <label for="city">City</label>
+                            <input type="text" id="city" name="city" value="<?=$data["city"]?>"/>
+                        </div> -->
+                        <div class="box">
+                            <label for="Address">Address:</label>
+                            <input type="text" id="address" name="address" placeholder="Enter Address" value="<?=$data["address"]?>"/>
+                        </div>
+                                
+                    </div>
+                    <div class="buttons">
+                        <input type="submit" name="submit" class="button" value="Update">
+                        <a href="<?php echo URLROOT;?>/Pages/Manager_Profile/manager" type="button" class="button">Cancel</a>
+                    </div>    
+                </form>
+            </div>    
+        </div>
+    </section>
 
-                </div>
-                <div class="line-1"></div>
-                <div class="edit-profile3">
-                    <div class="delete-profile">Change password</div>
-                </div>
-                <div class="group-138">
-                    <div class="group-135">
-                        <label for="CurrentPassword">Current Password:</label>
-                        <input type="CurrentPassword" id="pwd" name="pwd" />
-                    </div>
-                    <div class="group-136">
-                        <label for="NewPassword">New Password:</label>
-                        <input type="NewPassword" id="pwd" name="pwd" />
-                    </div>
-                    <div class="group-137">
-                        <label for="ConfirmPassword">Confirm New Password:</label>
-                        <input type="ConfirmPassword" id="ConfirmPassword" name="ConfirmPassword" />
-                    </div>
-                    <div class="form-submit1-btn">
-                        <input type="submit" name="submit" class="btn4" value="Change">
-                    </div>
-                </div> 
-        </form>
-    </div>
+    <script>
+        var loadFile = function (event) {
+        var image = document.getElementById("output");
+        image.src = URL.createObjectURL(event.target.files[0]);
+        };
+    </script>
 </body>
-
 </html>
+
+
