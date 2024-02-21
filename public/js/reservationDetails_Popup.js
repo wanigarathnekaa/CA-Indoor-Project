b8731025046f00471ce17e65e53ed073a985a5b6
MilanBhanuka <vgmbhanuka@gmail.com>
2024-02-21 21:27:39 +0530

    reschedule popup changed

--- public/js/reservationDetails_Popup.js
+++ public/js/reservationDetails_Popup.js
@@ -1,6 +1,6 @@
 let popup = document.getElementById("popup");
 let popupcontainer = document.getElementById("popupcontainer");
-let reschedulePopupContainer = document.getElementById("reschedulePopupContainer");
+// let reschedulePopupContainer = document.getElementById("reschedulePopupContainer");
 let deletePopup = document.getElementById("deletepopup");
 let numberOfDays = 0;
 let timeSlot = "";
@@ -67,18 +67,22 @@ function closeDeletePopup() {
 function openReschedulePopup() {
   // Hide the original popup and show the reschedulePopup
   console.log("Reschedule button clicked");
-  reschedulePopup.classList.add("open-popup");
-  reschedulePopupContainer.classList.add("open-popupcontainer");
+  reschedulePopup.classList.add("open-reschedulePopup");
+  popupcontainer.classList.add("open-popupcontainer");
+  popup.classList.remove("open-popup");
+  // reschedulePopupContainer.classList.add("open-popupcontainer");
 
-  popupcontainer.style.display = "none";
-  reschedulePopupContainer.style.display = "block";
+  // popupcontainer.style.display = "none";
+  // reschedulePopupContainer.style.display = "block";
   const r_timeSlot_r = document.querySelector(".r_timeSlot_r");
   r_timeSlot_r.textContent = timeSlot;
 }
 
 function closeReschedulePopup() {
-  reschedulePopup.classList.remove("open-popup");
-  reschedulePopupContainer.classList.remove("open-popupcontainer");
+  reschedulePopup.classList.remove("open-reschedulePopup");
+  popup.classList.add("open-popup");
+  // popupcontainer.classList.remove("open-popupcontainer");
+  // reschedulePopupContainer.classList.remove("open-popupcontainer");
   popupcontainer.style.display = "block";
   reschedulePopupContainer.style.display = "none";
 }
