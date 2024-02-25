3a11c2959425ee1021bdfd314c4af2a5db5a52b5
MilanBhanuka <vgmbhanuka@gmail.com>
2024-02-25 13:12:01 +0530

    change owner dashboard

--- app/views/Pages/Dashboard/owner.php
+++ app/views/Pages/Dashboard/owner.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Owner Dashboard</title>
-    <link rel="stylesheet" href="<?php echo URLROOT;?>/css/ownerDashboard.css">
+    <link rel="stylesheet" href="<?php echo URLROOT;?>/css/managerDashboard.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
 </head>
 
@@ -33,7 +33,7 @@
                 </div>
             </a>
 
-            <a class="card" href=#>
+            <a class="card" href="C&A_Indoor_Project/Pages/coachTable/owner">
                 <div>
                     <div class="numbers">15</div>
                     <div class="cardName">Coaches</div>
@@ -43,7 +43,7 @@
                 </div>
             </a>
 
-            <a class="card" href="#">
+            <a class="card" href="C&A_Indoor_Project/Pages/playerTable/owner">
                 <div>
                     <div class="numbers">80</div>
                     <div class="cardName">Players</div>
@@ -53,7 +53,7 @@
                 </div>
             </a>
 
-            <a class="card" href="#">
+            <a class="card" href="C&A_Indoor_Project/Pages/View_Advertisement/owner">
                 <div>
                     <div class="numbers">284</div>
                     <div class="cardName">Advertisement</div>
@@ -94,6 +94,39 @@
                     <i class="fa-solid fa-comments-dollar"></i>
                 </div>
             </a>
+        </div>
+
+        <!--Reservation Details -->
+        <div class="details">
+            <!-- Recent Reservations -->
+            <div class="tablediv">
+                <?php
+                        require APPROOT . '/views/Pages/Tables/dailyReservation.php';
+                ?>
+            </div>
+            
+            <!-- Calander -->
+            <div class="calanderdiv">
+                <iframe src="http://localhost/C&A_Indoor_Project/Pages/Calendar/User" frameborder="0"></iframe>                        
+            </div>
+        </div>
+
+        <!-- chart -->
+        <div class="charts">
+            <!-- Weekly Reservations chart -->
+            <?php
+                require APPROOT . '/views/Pages/Charts/weeklyReservationsChart.php';
+            ?>
+
+            <!-- Customers Count chart -->
+            <?php
+                require APPROOT . '/views/Pages/Charts/customersCountChart.php';
+            ?>
+
+            <!-- Reservations Net chart -->
+            <?php
+                require APPROOT . '/views/Pages/Charts/reservationsNetChart.php';
+            ?>
         </div>
 
 
