a39e107ae7739e592e4c9831a8a784b58069c189
wanigarathnekaa <kavisha.abhishake@gmail.com>
2024-04-13 02:23:40 +0530

    Charts

--- app/views/Pages/Charts/customersCountChart.php
+++ app/views/Pages/Charts/customersCountChart.php
@@ -1,25 +1,3 @@
-<?php
-$link = mysqli_connect("localhost", "root", "", "c&a_indoor_net");
-if ($link === false) {
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-
-//coach count
-$res_coaches = mysqli_query($link, "SELECT COUNT(*) AS coach_count FROM coaches");
-$row_coaches = mysqli_fetch_assoc($res_coaches);
-$coach_count = $row_coaches['coach_count'];
-
-//players + coaches count
-$res_total_users = mysqli_query($link, "SELECT COUNT(*) AS total_user_count FROM user");
-$row_total_users = mysqli_fetch_assoc($res_total_users);
-$total_user_count = $row_total_users['total_user_count'];
-
-//players count
-$player_count = $total_user_count - $coach_count;
-
-mysqli_close($link);
-?>
-
 <div class="chart2">
       <h1>Customers</h1>
       <canvas id="customerCountChart"></canvas>
@@ -27,7 +5,7 @@ mysqli_close($link);
 
 <script>
 var xValues = ["Players", "Coaches"];
-var yValues = [<?php echo $player_count ?>, <?php echo $coach_count ?>];
+var yValues = [<?php echo $data1["UserCount"]; ?>, <?php echo $data1["CoachCount"]; ?>];
 var barColors = ["#4F9DA9","#A7D8D7"];
 
 new Chart("customerCountChart", {
@@ -44,70 +22,5 @@ new Chart("customerCountChart", {
   }
 });
 </script>
-
-
-<!-- 
-class Model {
-    public function getPlayersAndCoachesCount() {
-        $link = mysqli_connect("localhost", "root", "", "c&a_indoor_net");
-        if ($link === false) {
-            die("ERROR: Could not connect. " . mysqli_connect_error());
-        }
-
-        $result = array();
-
-        $res_coaches = mysqli_query($link, "SELECT COUNT(*) AS coach_count FROM coaches");
-        $row_coaches = mysqli_fetch_assoc($res_coaches);
-        $coach_count = $row_coaches['coach_count'];
-
-        $res_total_users = mysqli_query($link, "SELECT COUNT(*) AS total_user_count FROM user");
-        $row_total_users = mysqli_fetch_assoc($res_total_users);
-        $total_user_count = $row_total_users['total_user_count'];
-
-        $player_count = $total_user_count - $coach_count;
-
-        $result['players'] = $player_count;
-        $result['coaches'] = $coach_count;
-
-        mysqli_close($link);
-
-        return $result;
-    }
-}
-
- -->
-
-
-
-
-
-<!-- 
-class Model {
-    public function getPlayersAndCoachesCount() {
-        $link = mysqli_connect("localhost", "root", "", "c&a_indoor_net");
-        if ($link === false) {
-            die("ERROR: Could not connect. " . mysqli_connect_error());
-        }
-
-        $result = array();
-
-        $res_coaches = mysqli_query($link, "SELECT COUNT(*) AS coach_count FROM coaches");
-        $row_coaches = mysqli_fetch_assoc($res_coaches);
-        $coach_count = $row_coaches['coach_count'];
-
-        $res_total_users = mysqli_query($link, "SELECT COUNT(*) AS total_user_count FROM user");
-        $row_total_users = mysqli_fetch_assoc($res_total_users);
-        $total_user_count = $row_total_users['total_user_count'];
-
-        $player_count = $total_user_count - $coach_count;
-
-        $result['players'] = $player_count;
-        $result['coaches'] = $coach_count;
-
-        mysqli_close($link);
-
-        return $result;
-    }
-} -->
 
 
