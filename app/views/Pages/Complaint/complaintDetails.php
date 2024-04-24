b0ef8e82b86abfcfbdf840b6b2ecd28a12856ce4
wanigarathnekaa <kavisha.abhishake@gmail.com>
2024-04-24 15:15:35 +0530

    View Complaints

--- app/views/Pages/Complaint/complaintDetails.php
+++ app/views/Pages/Complaint/complaintDetails.php
@@ -13,42 +13,48 @@
 <body>
       <!-- Sidebar -->
       <?php
-        $role = "Admin";
-        require APPROOT . '/views/Pages/Dashboard/header.php';
-        require APPROOT . '/views/Components/Side Bars/sideBar.php';
+      $role = "Admin";
+      require APPROOT . '/views/Pages/Dashboard/header.php';
+      require APPROOT . '/views/Components/Side Bars/sideBar.php';
       ?>
 
       <!-- Content -->
       <section class="home">
             <div class="carddiv">
                   <div class="card">
-                        <img src="" >
+                        <img src="">
                         <div class="hasini">
-                           <div class="right-side">
-          <div class="topic-text">Reply to the Complaint</div>
-          <form id="contact" action="<?php echo URLROOT; ?>/Complaint/sendEmail/<?php echo $data['complaint']->id; ?>" method="post">
-            <div class="input-box">
-                  <input type="text" id="name" name="name" placeholder="Enter your name" value="<?php echo $data['complaint']->name; ?>">
-            </div>
-            <div class="input-box">
-                  <input type="text" id="email" name="email" placeholder="Enter your email" value="<?php echo $data['complaint']->email; ?>">
-            </div>
-            <div class="input-box">
-            <label for="name">Complaint:</label>
+                              <div class="right-side">
+                                    <div class="topic-text">Reply to the Complaint</div>
+                                    <form id="contact" action="<?php echo URLROOT; ?>/Complaint/sendEmail/<?php echo $data['complaint']->id; ?>"
+                                          method="post">
+                                          <div class="input-box">
+                                                <input type="text" id="name" name="name" placeholder="Enter your name"
+                                                      value="<?php echo $data['complaint']->name; ?>">
+                                          </div>
+                                          <div class="input-box">
+                                                <input type="text" id="email" name="email"
+                                                      placeholder="Enter your email"
+                                                      value="<?php echo $data['complaint']->email; ?>">
+                                          </div>
+                                          <div class="input-box">
+                                                <label for="name">Complaint:</label>
 
-                 <textarea id="message" name="message1" placeholder="Enter your message"><?php echo $data['complaint']->message; ?></textarea>
-            </div>
-           
-            <div class="input-box" id="abc">
+                                                <textarea id="message" name="message1"
+                                                      placeholder="Enter your message"><?php echo $data['complaint']->message; ?></textarea>
+                                          </div>
 
-                  <textarea id="message" name="message" placeholder="Enter your Reply" value="" rows="6"></textarea>
-            </div>
-            <div class="button">
-              <input type="submit" value="Send Now" name="send">
-            </div>
-          </form>
-        </div>
-                              
+                                          <div class="input-box" id="abc">
+
+                                                <textarea id="message" name="message" placeholder="Enter your Reply"
+                                                      value="" rows="6"></textarea>
+                                          </div>
+                                          <div class="button">
+                                                <input type="submit" value="Send Now" name="send">
+                                          </div>
+                                    </form>
+                              </div>
+
                         </div>
                   </div>
             </div>
